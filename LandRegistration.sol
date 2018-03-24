e29bf2942d2aa82550db2b349d78fbf9f1c85c20
Gopal Ojha <gopalonevirus@gmail.com>
2018-03-24 15:49:39 +0545

    fixes issue of event

--- LandRegistration.sol
+++ LandRegistration.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity 0.4.21;
 contract LandRegistration{
 struct LandDetails{
     //AccountID is account address
@@ -18,22 +18,23 @@ struct LandDetails{
     bool Activestatus;
     address PreviousOwnerID;
 }
+bytes32 public donemessage = "Preocess Done";
 address public government;
 modifier only_gov(){
     require(government == msg.sender);
     _;
 }
-event VerifyLog(bytes32, address, bytes32, bytes32, bytes32, bytes32, bool, bool);
-event PropertyRegisterLog(bytes32, address, bytes32, bytes32, bytes32, bytes32, bool, bool);
-event TransferOwnerLog(bytes32, address, bytes32, bytes32, bytes32, bytes32, bool, bool,address);
+event VerifyLog(bool);
+event PropertyRegisterLog(bool);
+event TransferOwnerLog(bool);
 mapping(
     bytes32 => mapping(
        address => LandDetails 
         )
     ) landdetails;
 
-    function LandRegistration()public{
-        government = msg.sender;
+    function LandRegistration(address _governmentaddress)public{
+              government = _governmentaddress;
     }
     //Register all details of ownner and property  
 
@@ -50,17 +51,7 @@ mapping(
     landdetails[_propertyID][_accountID].Activestatus = false;
     landdetails[_propertyID][_accountID].PreviousOwnerID = 0x0 ;
 
-    emit PropertyRegisterLog(
-            landdetails[_propertyID][_accountID].PropertyID, 
-            landdetails[_propertyID][_accountID].AccountID,
-            landdetails[_propertyID][_accountID].Ownername,
-            landdetails[_propertyID][_accountID].OwnerAddress, 
-            landdetails[_propertyID][_accountID].GovID, 
-            landdetails[_propertyID][_accountID].Pow,
-            landdetails[_propertyID][_accountID].Registered_status,
-            landdetails[_propertyID][_accountID].Activestatus
-            
-            );
+    emit PropertyRegisterLog(landdetails[_propertyID][_accountID].Registered_status);
     
     }
     function display(bytes32 _propertyID, address _accountID)public view returns(bytes32, address,bytes32,bool,bool,address){
@@ -71,6 +62,7 @@ mapping(
             landdetails[_propertyID][_accountID].Registered_status,
             landdetails[_propertyID][_accountID].Activestatus,
             landdetails[_propertyID][_accountID].PreviousOwnerID
+            
           );
         }
     
@@ -82,16 +74,7 @@ mapping(
             landdetails[_propertyID][_accountID].Activestatus = true;
             
         
-        emit VerifyLog(
-            landdetails[_propertyID][_accountID].PropertyID, 
-            landdetails[_propertyID][_accountID].AccountID,
-            landdetails[_propertyID][_accountID].Ownername,
-            landdetails[_propertyID][_accountID].OwnerAddress, 
-            landdetails[_propertyID][_accountID].GovID, 
-            landdetails[_propertyID][_accountID].Pow,
-            landdetails[_propertyID][_accountID].Registered_status,
-            landdetails[_propertyID][_accountID].Activestatus
-            );
+        emit VerifyLog(landdetails[_propertyID][_accountID].Activestatus);
     }
     function transferOwner(address trfrom_accountid, address trto_accountid, bytes32 _propertyID, bytes32 _ownername, bytes32 _owneraddr, bytes32 _govID)public {
     //transfers only by owner of the asset
@@ -116,17 +99,7 @@ mapping(
     landdetails[_propertyID][trto_accountid].PreviousOwnerID = landdetails[_propertyID][trfrom_accountid].AccountID; 
     //require check whether tha land is properly transfered 
     //require(landdetails[_propertyID][trto_accountid].Activestatus);
-    emit TransferOwnerLog(
-            landdetails[_propertyID][trto_accountid].PropertyID, 
-            landdetails[_propertyID][trto_accountid].AccountID,
-            landdetails[_propertyID][trto_accountid].Ownername,
-            landdetails[_propertyID][trto_accountid].OwnerAddress, 
-            landdetails[_propertyID][trto_accountid].GovID, 
-            landdetails[_propertyID][trto_accountid].Pow,
-            landdetails[_propertyID][trto_accountid].Registered_status,
-            landdetails[_propertyID][trto_accountid].Activestatus,
-            landdetails[_propertyID][trto_accountid].PreviousOwnerID
-            );
+    emit TransferOwnerLog(landdetails[_propertyID][trto_accountid].Activestatus);
     }    
     
 } 
